3d27b2814d4abc4de951c2d57b168c8b8d1421d3
Anibal Ventura <anibalventura@icloud.com>
2023-01-04 22:00:04 -0400

    Add middlewares.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import config from './config';
+import cors from 'cors';
 import productRoutes from './routes/products.route';
 import updateRoutes from './routes/update.route';
 import updatepointRoutes from './routes/updatepoint.route';
@@ -13,7 +14,10 @@ dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 app.set('port', config.port);
 
 // Entry middleware's.
+app.use(cors())
 loggerMiddleware(app);
+app.use(express.json()); // For parsing application/json
+app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 
 // Routes.
 app.get('/api', (_, res) => res.send({ app: 'ChangeLog API'}));
