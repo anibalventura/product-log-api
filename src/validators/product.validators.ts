5406dda2970328837c00edc46ebea2bd3962e0b3
Anibal Ventura <anibalventura@icloud.com>
2023-01-08 12:50:27 -0400

    Fix value request.

--- src/validators/product.validators.ts
+++ src/validators/product.validators.ts
@@ -1,10 +1,9 @@
-import { getProduct } from './../controllers/products.controller';
-import { body, header } from 'express-validator';
+import { body, param } from 'express-validator';
 import { inputValidatorMiddleware } from '../middlewares/inputValidator.middleware';
 
 export default {
-  getProduct: [header('id').notEmpty(), inputValidatorMiddleware],
+  getProduct: [param('id').notEmpty(), inputValidatorMiddleware],
   createProduct: [body('name').isString(), inputValidatorMiddleware],
-  deleteProduct: [header('id').notEmpty(), inputValidatorMiddleware],
+  deleteProduct: [param('id').notEmpty(), inputValidatorMiddleware],
   updateProduct: [body('name').isString(), inputValidatorMiddleware],
 };
